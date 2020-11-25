b52948f6351cfd14819b86656bfd6317195af075
Thomas <banana439monkey@gmail.com>
2020-11-25 13:50:09 +0000

    oop

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 import mysql.connector # https://www.geeksforgeeks.org/mysql-connector-python-module-in-python/ along with line 7
 dbusername = open("username.txt", "r").read()
 dbpassword = open("password.txt", "r").read()
-db = open("db.txt", "r").read()
+dbname = open("db.txt", "r").read()
 songnames = open("songwriters.txt", "r").read()
 songwriters = open("songwriters.txt", "r").read()
-mysql.connector.connect(host='localhost', database=db, user=dbusername password=dbpassword)
+db = mysql.connector.connect(host='localhost', database=dbname, user=dbusername password=dbpassword)
+if 
